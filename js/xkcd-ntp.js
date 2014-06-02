1753ae3cc0b37328bcb34af658ca07e8bdaf9489
Nick Morgan <NickMorgan@quickenloans.com>
2014-06-02 08:43:25 -0400

    random button functionality and comic number

--- js/xkcd-ntp.js
+++ js/xkcd-ntp.js
@@ -1,13 +1,27 @@
 // Wait for request and then populate title and image
 
-function reqListener () {
+function reqListener() {
   console.log(this.response);
-  document.getElementById("comicTitle").innerHTML=this.response.title;
-  document.getElementById("comic").src=this.response.img;
-  document.getElementById("comic").alt=this.response.title;
-  document.getElementById("comic").title=this.response.alt;
+  document.getElementById("comicTitle").innerHTML = this.response.title;
+  document.getElementById("comicNumber").innerHTML = this.response.num;
+  document.getElementById("comic").src = this.response.img;
+  document.getElementById("comic").alt = this.response.title;
+  document.getElementById("comic").title = this.response.alt;
 
   curComic = this.response.num;
+}
+
+function setup() {
+  console.log(this.response);
+  document.getElementById("comicTitle").innerHTML = this.response.title;
+  document.getElementById("comicNumber").innerHTML = this.response.num;
+  document.getElementById("comic").src = this.response.img;
+  document.getElementById("comic").alt = this.response.title;
+  document.getElementById("comic").title = this.response.alt;
+
+  curComic = this.response.num;
+  recComic = this.response.num;
+  
 }
 
 function getFirst() {
@@ -23,6 +37,15 @@ function getPrev() {
   Req.responseType = "json";
   Req.onload = reqListener;
   Req.open("GET", "http://xkcd.com/" + (curComic-1) + "/info.0.json", true);
+  Req.send();
+}
+
+function getRandom() {
+  randComic = Math.floor((Math.random() * recComic) + 1);
+  var Req = new XMLHttpRequest();
+  Req.responseType = "json";
+  Req.onload = reqListener;
+  Req.open("GET", "http://xkcd.com/" + randComic + "/info.0.json", true);
   Req.send();
 }
 
@@ -48,7 +71,7 @@ var recComic = null;
 var curComic = null;
 
 oReq.responseType = "json";
-oReq.onload = reqListener;
+oReq.onload = setup;
 oReq.open("GET", "http://xkcd.com/info.0.json", true);
 oReq.send();
 
@@ -60,8 +83,10 @@ var plt = document.getElementById("prevTop");
 plt.addEventListener("click", getPrev, false);
 var plb = document.getElementById("prevBottom");
 plb.addEventListener("click", getPrev, false);
-// var rl = document.getElementById("random");
-// rl.addEventListener("click", getRandom, false);
+var rlt = document.getElementById("randomTop");
+rlt.addEventListener("click", getRandom, false);
+var rlb = document.getElementById("randomBottom");
+rlb.addEventListener("click", getRandom, false);
 var nlt = document.getElementById("nextTop");
 nlt.addEventListener("click", getNext, false);
 var nlb = document.getElementById("nextBottom");
