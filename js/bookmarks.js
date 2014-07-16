235483b48e194c91e0d8585795cc4345a8b6499b
nmorgan1 <nickmorgan@quickenloans.com>
2014-07-16 15:16:05 -0400

    Fix bug where bookmark folders would break bookmark section

--- js/bookmarks.js
+++ js/bookmarks.js
@@ -4,17 +4,29 @@ chrome.bookmarks.getChildren('1', function (results)
 		var bookmarkList = document.createElement("ul");
 		bookmarkContainer.appendChild(bookmarkList);
 
-		for (i = 0; i < results.length; ++i) {
-			if (results[i].url.indexOf("chrome://") != -1) {
+		printBookmarks(results, bookmarkList);
+	});
+
+function printBookmarks(bookmarkArray, bookmarkList)
+	{
+		for (i = 0; i < bookmarkArray.length; ++i) {
+			if (bookmarkArray[i].url === undefined) {
+				chrome.bookmarks.getChildren(bookmarkArray[i].id, function (results)
+					{
+						printBookmarks(results, bookmarkList);
+					});
+			}
+			else if (bookmarkArray[i].url.indexOf("chrome://") == 0) {
 				continue;
 			}
-			var bookmark = document.createElement("li");
-			var link = document.createElement("a");
-			link.innerHTML = results[i].title;
-			link.href = results[i].url;
-			
-			bookmark.appendChild(link);
-			bookmarkList.appendChild(bookmark);
-			console.log(results[i]);
+			else {
+				var bookmark = document.createElement("li");
+				var link = document.createElement("a");
+				link.innerHTML = bookmarkArray[i].title;
+				link.href = bookmarkArray[i].url;
+				
+				bookmark.appendChild(link);
+				bookmarkList.appendChild(bookmark);
+			}
 		}
-	});
+	}
