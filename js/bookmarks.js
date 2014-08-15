4a51978d9ca869fb3d6c86935860ef5f70f5b8e3
nmorgan1 <nickmorgan@quickenloans.com>
2014-08-15 14:54:30 -0400

    Not sure whats happening but updated

--- js/bookmarks.js
+++ js/bookmarks.js
@@ -1,32 +1,38 @@
-chrome.bookmarks.getChildren('1', function (results)
-	{
-		var bookmarkContainer = document.getElementById("bookmarkContainer");
-		var bookmarkList = document.createElement("ul");
-		bookmarkContainer.appendChild(bookmarkList);
+chrome.bookmarks.getTree(function (results)
+{
+	var bookmarkContainer = document.getElementById("bookmarkContainer");
+	var bookmarkList = document.createElement("ul");
+	bookmarkContainer.appendChild(bookmarkList);
 
+	printBookmarks(results, bookmarkList);
+});
+
+function folderTrav(item, bookmarkList)
+{
+	chrome.bookmarks.getChildren(item.id, function (results) {
 		printBookmarks(results, bookmarkList);
 	});
+}
 
 function printBookmarks(bookmarkArray, bookmarkList)
-	{
-		for (i = 0; i < bookmarkArray.length; ++i) {
-			if (bookmarkArray[i].url === undefined) {
-				chrome.bookmarks.getChildren(bookmarkArray[i].id, function (results)
-					{
-						printBookmarks(results, bookmarkList);
-					});
-			}
-			else if (bookmarkArray[i].url.indexOf("chrome://") == 0) {
-				continue;
-			}
-			else {
-				var bookmark = document.createElement("li");
-				var link = document.createElement("a");
-				link.innerHTML = bookmarkArray[i].title;
-				link.href = bookmarkArray[i].url;
-				
-				bookmark.appendChild(link);
-				bookmarkList.appendChild(bookmark);
-			}
+{
+	for (i = 0; i < bookmarkArray.length; ++i) {
+		var bookmark = document.createElement("li");
+		var link = document.createElement("a");
+		// If encountered element is a folder, go inside
+		if (bookmarkArray[i].url === undefined) {
+			folderTrav(bookmarkArray[i], bookmarkList);
+		}
+		// If it is a bookmark to Chrome, move on
+		else if (bookmarkArray[i].url.indexOf("chrome://") == 0) {
+			continue;
+		}
+		// Else print
+		else {
+			link.innerHTML = bookmarkArray[i].title;
+			link.href = bookmarkArray[i].url;
+			bookmark.appendChild(link);
+			bookmarkList.appendChild(bookmark);
 		}
 	}
+}
